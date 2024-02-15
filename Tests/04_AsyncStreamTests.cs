8e829d5b72bae071d84a177e0ad0a2e02fd83798
Bnaya Eshet <bnayaes@payoneer.com>
2024-02-15 16:51:40 +0200

    up to time abstraction

--- Tests/04_AsyncStreamTests.cs
+++ Tests/04_AsyncStreamTests.cs
@@ -1,4 +1,5 @@
 using FakeItEasy;
+using System;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using Xunit.Abstractions;
@@ -8,7 +9,7 @@ namespace Tests;
 public class AsyncStreamTests
 {
     private readonly ITestOutputHelper _logger;
-    
+
     public AsyncStreamTests(ITestOutputHelper logger)
     {
         _logger = logger;
@@ -22,7 +23,7 @@ public class AsyncStreamTests
         _logger.WriteLine($"duration: {duration}");
         var sw = Stopwatch.StartNew();
         var ct = new CancellationTokenSource(duration).Token;
-        var result = await AsyncStreamExtensiosn.CreateAsyncStream(ct) // pass cancellation
+        var result = await AsyncStreamExtensiosn.CreateAsyncStream(cancellationToken: ct) // pass cancellation
                                                 .Log(_logger, sw)
                                                 .Count();
 
@@ -33,7 +34,7 @@ public class AsyncStreamTests
     [Theory]
     [InlineData(100 * 3 + 50, 3)]
     [InlineData(100 * 5 + 50, 5)]
-    public async Task AsynStreamsTest(int duration, int expectedLength)
+    public async Task AsynStreamsest(int duration, int expectedLength)
     {
         _logger.WriteLine($"duration: {duration}");
         var stream = AsyncStreamExtensiosn.CreateAsyncStream();
@@ -51,21 +52,119 @@ public class AsyncStreamTests
             _logger.WriteLine($"result: {result}");
             Assert.Equal(expectedLength, result);
             sw.Stop();
-        }         
+        }
+    }
+
+    #region TimeManagement
+
+    sealed record TimeManagement: IAsyncDisposable
+    {
+        private readonly Task _triggering;
+
+        #region Ctor
+
+        public TimeManagement(int expectedLength)
+        {
+            TimeProvider = A.Fake<TimeProvider>();
+
+            #region A.CallTo(() => timeProvider.CreateTimer(...)
+
+            A.CallTo(() => TimeProvider.CreateTimer(A<TimerCallback>.Ignored, A<object?>.Ignored, A<TimeSpan>.Ignored, A<TimeSpan>.Ignored))
+                .Invokes((TimerCallback cb, object? state, TimeSpan dutime, TimeSpan period) =>
+                {
+                    if (state is CancellationTokenSource)
+                    {
+                        CancellationTimerCallback = cb;
+                        CancellationState = state;
+                    }
+                    else
+                    {
+                        StreamTimerCallback = cb;
+                        StreamState = state;
+                        Sync.Release();
+                    }
+                });
+
+            #endregion // A.CallTo(() => timeProvider.CreateTimer(...)
+
+            _triggering = InitAsync(expectedLength);
+        }
+
+        #endregion // Ctor
+
+        #region InitAsync
+
+        private Task InitAsync(int expectedLength)
+        { 
+            return Task.Run(async () =>
+            {
+                await Sync.WaitAsync();
+                for (int i = 0; i < expectedLength; i++)
+                {
+                    StreamTimerCallback?.Invoke(StreamState); // async stream
+                    await Task.Delay(1);
+                }
+                CancellationTimerCallback?.Invoke(CancellationState); // cancellation
+                await Task.Delay(1);
+                StreamTimerCallback?.Invoke(StreamState); // async stream
+            });
+        }
+
+        #endregion // InitAsync
+
+        public TimerCallback? StreamTimerCallback { get; set; }
+        public object? StreamState { get; set; }
+        public TimerCallback? CancellationTimerCallback { get; set; }
+        public object? CancellationState { get; set; }
+        public TimeProvider TimeProvider { get; }
+        public SemaphoreSlim Sync { get; } = new SemaphoreSlim(1);
+
+        public async ValueTask DisposeAsync()
+        {
+            await _triggering;
+        }
+    }
+
+    #endregion // TimeManagement
+
+    [Theory]
+    [InlineData(100 * 3 + 50, 3)]
+    [InlineData(100 * 5 + 50, 5)]
+    public async Task AsynStream_TimeAbstraction_Test(int duration, int expectedLength)
+    {
+        await using var timeManagement = new TimeManagement(expectedLength); // IAsyncDisposable
+        await timeManagement.Sync.WaitAsync();
+
+        _logger.WriteLine($"duration: {duration}");
+        var sw = Stopwatch.StartNew();
+        var ct = new CancellationTokenSource(
+                        TimeSpan.FromMilliseconds(duration),
+                        timeManagement.TimeProvider).Token;
+        var result = await AsyncStreamExtensiosn.CreateAsyncStream(timeManagement.TimeProvider, ct) // pass cancellation
+                                                .Log(_logger, sw)
+                                                .Count();
+
+        _logger.WriteLine($"result: {result}");
+        Assert.Equal(expectedLength, result);
     }
 }
 
 public static class AsyncStreamExtensiosn
 {
     public static async IAsyncEnumerable<int> CreateAsyncStream(
+                        TimeProvider? timeProvider = default,
                         [EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
+        timeProvider = timeProvider ?? TimeProvider.System;
         int i = 0;
         while (!cancellationToken.IsCancellationRequested)
         {
             try
             {
-                await Task.Delay(100, cancellationToken);
+                await Task.Delay(
+                            TimeSpan.FromMilliseconds(100),
+                            timeProvider,
+                            cancellationToken);
             }
             catch (OperationCanceledException cancelled)
             {
